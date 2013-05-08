a117461636ecdb0f1fec5a1c9f115a24e5ed04c2
YCB <info@yourcustomblog.com>
2013-05-07 18:08:41 -0700

    read/write permission for all files
    fix display problem for local WP dashboard by changing permissions
    recursively

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'obc');
+define('DB_NAME', 'aaf');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ADNJ1807');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:/tmp/mysql.sock');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
