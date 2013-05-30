e6fdb0cd8a9959b7f3f987bf2a0f25090dddfd56
Dario Novoa <darionovoa@ideartte.com>
2013-05-30 15:30:13 -0700

    Blog sidebar
    Blog sidebar should now appear on the dashboard.. This is not yet
    implemented on the front-end

--- wp-content/themes/aaf/functions.php
+++ wp-content/themes/aaf/functions.php
@@ -135,6 +135,15 @@ class AAF {
             'before_title' => '<h3 class="widget-title">',
             'after_title' => '</h3>',
         ) );
+
+        register_sidebar( array(
+            'name' => __( 'Blog Sidebar', 'AAF' ),
+            'id' => 'blog',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h3 class="widget-title">',
+            'after_title' => '</h3>',
+        ) );
     }
 
     /**
