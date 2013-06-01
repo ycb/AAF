bb55e3cb5194a0899c1a8cf8feb6ca2ad38b45e0
YCB <info@yourcustomblog.com>
2013-06-01 16:50:38 -0700

    get rid of that ugly Category Archives label

--- wp-content/themes/aaf/archive.php
+++ wp-content/themes/aaf/archive.php
@@ -19,15 +19,15 @@ get_header();
                 <h1 class="page-title">
                     <?php
                     if ( is_category() ) {
-                        printf( __( 'Category Archives: %s', 'AAF' ), '<span>' . single_cat_title( '', false ) . '</span>' );
+                        printf( __( '%s', 'AAF' ), '<span>' . single_cat_title( '', false ) . '</span>' );
                     } elseif ( is_tag() ) {
-                        printf( __( 'Tag Archives: %s', 'AAF' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+                        printf( __( '%s', 'AAF' ), '<span>' . single_tag_title( '', false ) . '</span>' );
                     } elseif ( is_author() ) {
                         /* Queue the first post, that way we know
                          * what author we're dealing with (if that is the case).
                          */
                         the_post();
-                        printf( __( 'Author Archives: %s', 'AAF' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
+                        printf( __( '%s', 'AAF' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
                         /* Since we called the_post() above, we need to
                          * rewind the loop back to the beginning that way
                          * we can run the loop properly, in full.
