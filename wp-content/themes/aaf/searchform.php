8f03c5cffd16713af6ff4cb0e7ea2034f164b7a2
Dario Novoa <darionovoa@ideartte.com>
2013-06-03 16:03:09 -0700

    Footer search, footer social icons, more styling and details.
    Closes #27
    
    - Adds footer search box and social icons
    - Styles all of the above
    - Custom searchform.

--- wp-content/themes/aaf/searchform.php
+++ wp-content/themes/aaf/searchform.php
@@ -8,6 +8,7 @@
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
     <label for="s" class="assistive-text"><?php _e( 'Search', 'AAF' ); ?></label>
-    <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'AAF' ); ?>" />
-    <input type="submit" class="submit btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'AAF' ); ?>" />
+    <i class="icon-search icon-white"></i>
+    <input type="text" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="&nbsp;&nbsp;&nbsp;&nbsp;<?php esc_attr_e( 'Search &hellip;', 'AAF' ); ?>" />
+    <input type="submit" class="submit btn" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'GO', 'AAF' ); ?>" />
 </form>
