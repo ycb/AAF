23b3b56921bb5dd19b5e3c03dd6447ba02e4541a
Dario Novoa <darionovoa@ideartte.com>
2013-06-03 14:06:49 -0700

    Removed comments from the template.
    - comments.php is still there in case at some point we want to have
    this enabled.

--- wp-content/themes/aaf/single.php
+++ wp-content/themes/aaf/single.php
@@ -17,12 +17,6 @@ get_header();
 
             <?php AAF_content_nav( 'nav-below' ); ?>
 
-            <?php
-            // If comments are open or we have at least one comment, load up the comment template
-            if ( comments_open() || '0' != get_comments_number() )
-                comments_template( '', true );
-            ?>
-
         <?php endwhile; // end of the loop. ?>
 
     </div><!-- #content .site-content -->
