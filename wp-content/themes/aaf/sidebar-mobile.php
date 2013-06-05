49be9fa506b656d2ad59f3ae44f16bc25161cdb6
YCB <info@yourcustomblog.com>
2013-06-04 17:00:09 -0700

    mobile - footer fixes

--- wp-content/themes/aaf/sidebar-mobile.php
+++ wp-content/themes/aaf/sidebar-mobile.php
@@ -16,7 +16,7 @@
         <aside id="nav_menu-3" class="widget widget_nav_menu">
             <div class="menu-mobile-bottom-nav-container">
                 <ul id="menu-mobile-bottom-nav" class="menu">
-                    <li><a href="#">GET DIRECTIONS</a><br /><small>OMEGA BOYS CLUB IS LOCATED AT <br />1060 TENNESSEE STREET, SAN FRANCISCO, CA 94107</small></li>
+                    <li><a href="https://maps.google.com/maps?f=q&source=embed&hl=en&geocode=&q=Omega+Boys+Club,+1060+Tennessee+Street,+San+Francisco,+CA&aq=0&oq=ome&sll=37.269174,-119.306607&sspn=12.3355,20.061035&ie=UTF8&hq=Omega+Boys+Club,+1060+Tennessee+Street,+San+Francisco,+CA&hnear=&radius=15000&ll=37.758517,-122.389434&spn=0.095815,0.130291&t=m&z=13&iwloc=A&cid=6160364427089974319">GET DIRECTIONS</a><br /><small>OMEGA BOYS CLUB IS LOCATED AT <br />1060 TENNESSEE STREET, SAN FRANCISCO, CA 94107</small></li>
                 </ul>
             </div>
         </aside>
