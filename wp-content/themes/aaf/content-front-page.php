4f01d6a0cb6d002664c2aef5d4adc815edc495f8
Dario Novoa <darionovoa@ideartte.com>
2013-06-04 17:43:04 -0700

    Show the Social btns on the front page post.

--- wp-content/themes/aaf/content-front-page.php
+++ wp-content/themes/aaf/content-front-page.php
@@ -18,8 +18,10 @@
     <div class="entry-content">
         <?php 
         	$content = get_the_content('');
-        	echo wp_trim_words( $content, $num_words = 55, '' ) . '<br><a href="' . get_permalink() . '"><span class="meta-nav btn btn-danger">READ MORE</span></a>';
+        	echo wp_trim_words( $content, $num_words = 55, '' ) . '<br><br><br><a href="' . get_permalink() . '"><span class="meta-nav btn btn-danger pull-right">READ MORE</span></a>';
         ?>
+        <span class='st_facebook_hcount' displayText='Facebook'></span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class='st_twitter_hcount' displayText='Tweet'></span>
         <?php wp_link_pages( array('before' => '<div class="page-links">' . __( 'Pages:', 'AAF' ), 'after' => '</div>') ); ?>
         <?php edit_post_link( __( 'Edit', 'AAF' ), '<span class="edit-link">', '</span>' ); ?>
     </div><!-- .entry-content -->
