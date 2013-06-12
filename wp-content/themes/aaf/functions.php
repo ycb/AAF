10104de80e99d7ebca5efe208c192622bece499c
Dario Novoa <darionovoa@ideartte.com>
2013-06-12 12:19:25 -0700

    Filter to add the class thickbox
    Filter to add the class thickbox to any image added to a post

--- wp-content/themes/aaf/functions.php
+++ wp-content/themes/aaf/functions.php
@@ -356,3 +356,20 @@ add_filter( 'wp_nav_menu_items', 'nav_menu_first_last' );
 
 //sidebar shortcode
 add_filter('widget_text', 'do_shortcode');
+
+/**
+ * Attach a class to linked images' parent anchors
+ * e.g. a img => a.img img
+ */
+function add_linked_images_class($html, $id, $caption, $title, $align, $url, $size, $alt = '' ){
+    $classes = 'thickbox'; // separated by spaces, e.g. 'img image-link'
+
+    // check if there are already classes assigned to the anchor
+    if ( preg_match('/<a.*? class=".*?">/', $html) ) {
+        $html = preg_replace('/(<a.*? class=".*?)(".*?>)/', '$1 ' . $classes . '$2', $html);
+    } else {
+        $html = preg_replace('/(<a.*?)>/', '$1 class="' . $classes . '" >', $html);
+    }
+    return $html;
+}
+add_filter('image_send_to_editor','give_linked_images_class',10,8);
